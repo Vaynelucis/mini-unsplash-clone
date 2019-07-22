1a7cbb714b299eee5e89fc8183a6796d8efb839f
Benard Obinna <bernardobinna2@gmail.com>
2019-07-22 18:42:31 +0100

    added the search nav, search styles and restructured files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Home
   }
-}
+};
 </script>
 
-<style>
+<style >
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  /* text-align: center; */
+  /* color: #2c3e50; */
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="body">
+    <div class="search">
+      <div class="search-bar">
+        <!-- <span class="fa fa-search" id="search-icon"></span> -->
+        <i class="fa fa-search fa-lg" aria-hidden="true"></i>
+        <input type="text" id="search-input" placeholder="Search for photo">
+      </div>
+    </div>
+    <div class="main-body"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+@import "../styles/main.css";
+</style>
