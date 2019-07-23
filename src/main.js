341410f2d4f9dce1e7e3ddc05a9168195617371d
Benard Obinna <bernardobinna2@gmail.com>
2019-07-23 13:22:53 +0100

    added getLatestImages method

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { VueMasonryPlugin } from "vue-masonry";
+import axios from "axios";
 
 Vue.use(VueMasonryPlugin);
 Vue.config.productionTip = false;
 
 new Vue({
+  axios,
   render: h => h(App)
 }).$mount("#app");
